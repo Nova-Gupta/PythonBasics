d6e5becf28ee8f220bd35697e0638c03da9c46fb
Riya Gupta <ngnova530@gmail.com>
2026-01-23 14:21:39 +0530

    added loops file

--- loops.py
+++ loops.py
@@ -7,3 +7,11 @@ i = 0
 while i < 5:
     print("Count:", i)
     i += 1
+
+#loop with break and continue
+for num in range(10):
+    if num % 2 == 0:
+        continue  # Skip even numbers
+    if num > 7:
+        break  # Stop the loop if number is greater than 7
+    print("Odd number less than 8:", num)
