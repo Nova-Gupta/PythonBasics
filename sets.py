1d96cf50ca1747491f94fedff8b3a062ea655481
Riya Gupta <ngnova530@gmail.com>
2026-01-24 15:44:13 +0530

    added sets file

---
+++ sets.py
@@ -0,0 +1,11 @@
+numbers = {1, 2, 3, 4, 5}
+print(numbers)
+numbers.add(6)
+print(numbers)
+numbers.remove(3)
+print(numbers)
+n = {5, 6, 7, 8, 9}
+print(numbers.intersection(n))
+print(numbers.union(n))
+print(numbers.difference(n))
+print(n.difference(numbers))
